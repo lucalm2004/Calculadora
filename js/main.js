dc9f5a4a9b8fc5beca36596ca959ab8654bdc7ab
lucalm2004 <lucalm2004@gmail.com>
2022-04-22 16:21:26 +0200

    ver1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
 function calcular() {
     var operacion = document.getElementById("operacion").value;
-    alert('Voy a : '+operacion);
+    var valor1 = document.getElementById("valor1").value;
+    var valor2 = document.getElementById("valor2").value;
+
+    if(operacion=="sumar"){
+       var resultado =  Number(valor1)+Number(valor2)
+    }else if(operacion=='restar'){
+        var resultado = Number(valor1)-Number(valor2)
+    }else if(operacion=='multiplicar'){
+        var resultado = Number(valor1)*Number(valor2)
+    }else{
+        var resultado = Number(valor1)/Number(valor2)
+    };
+
+    var parrafo = document.getElementById('resultado')
+    parrafo.innerHTML=resultado
+    //alert('Voy a '+operacion+'los valores'+valor1 + ' y '+valor2);
+
+   
+
 }
